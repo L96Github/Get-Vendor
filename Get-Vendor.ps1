f040c9dfdf948c7cfc32710ea8b84653ccae9cf7
L96Github <luis-ivan93@hotmail.com>
2018-07-27 17:36:33 +0100

    0.0.2 - Adds try..catch
    - Adds Try..Catch to display "Not Found" without throwing error.
    - Displays inputted MAC even if error is catched

--- Get-Vendor.ps1
+++ Get-Vendor.ps1
@@ -3,13 +3,20 @@ param (
 )
 $escMac = [Uri]::EscapeDataString($mac)
 
-$result = (Invoke-WebRequest https://api.macvendors.com/$escMac).Content
 
-If(-Not $result){
-    Write-Output "Not Found"
-}
-Else{
+try {
+    $result = (Invoke-WebRequest https://api.macvendors.com/$escMac).Content
+
     Write-Output "Mac: $mac"
-    
-    Write-Output "Vendor: $result"
+
+    If(-Not $result){
+        Write-Output "Not Found"
+    }
+    Else{
+        Write-Output "Vendor: $result"
+    }
+} catch {
+    Write-Output "Mac: $mac"
+    Write-Output $_.Exception.Response.StatusDescription
 }
+
